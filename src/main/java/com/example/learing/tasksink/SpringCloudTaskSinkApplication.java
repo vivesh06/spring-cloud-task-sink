7caddcb0a55a28dfb991003e60ee75cc5f95a1fa
vivesh <vivesh06@gmail.com>
2017-12-06 10:32:21 +0200

    fix cinfig

--- src/main/java/com/example/learing/tasksink/SpringCloudTaskSinkApplication.java
+++ src/main/java/com/example/learing/tasksink/SpringCloudTaskSinkApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.task.launcher.annotation.EnableTaskLauncher;
 
 @SpringBootApplication
-@EnableTaskLauncher()
+@EnableTaskLauncher
 public class SpringCloudTaskSinkApplication {
 
 	public static void main(String[] args) {
